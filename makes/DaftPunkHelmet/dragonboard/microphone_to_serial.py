3e4a17e20652fb2b3401ba0f947f6a55c67fb730
Estefanniegg <estefanniegg@gmail.com>
2018-08-01 20:38:02 -0500

    Daft Punk Helmet code

---
+++ makes/DaftPunkHelmet/dragonboard/microphone_to_serial.py
@@ -0,0 +1,116 @@
+import sys
+import pyaudio
+from struct import unpack
+import numpy as np
+import serial
+import time
+import threading
+
+spectrum  = [1,1,1,3,3,3,2,2]
+matrix    = [0,0,0]
+power     = []
+weighting = [2,32,64] 
+ser = serial.Serial('/dev/tty96B0', 9600)
+running = True
+def serial_thread():
+    global matrix
+
+    while running:
+#        print ser.readline()
+        time.sleep(0.07)
+        levels = ""
+        for each in matrix:
+            levels += str(each) + ","
+
+        levels = "s" + levels[:-1]
+
+        print levels
+        ser.write(levels) 
+
+def list_devices():
+    # List all audio input devices
+    p = pyaudio.PyAudio()
+    i = 0
+    n = p.get_device_count()
+    while i < n:
+        dev = p.get_device_info_by_index(i)
+        if dev['maxInputChannels'] > 0:
+           print(str(i)+'. '+dev['name'])
+        i += 1
+
+# Audio setup
+no_channels = 1
+sample_rate = 44100
+
+# Chunk must be a multiple of 8
+# NOTE: If chunk size is too small the program will crash
+# with error message: [Errno Input overflowed]
+chunk = 3072 
+
+list_devices()
+# Use results from list_devices() to determine your microphone index
+device = 0 
+
+print device
+
+p = pyaudio.PyAudio()
+stream = p.open(format = pyaudio.paInt16,
+                channels = no_channels,
+                rate = sample_rate,
+                input = True,
+                frames_per_buffer = chunk,
+                input_device_index = device)
+
+
+# Return power array index corresponding to a particular frequency
+def piff(val):
+    return int(2*chunk*val/sample_rate)
+   
+def calculate_levels(data, chunk,sample_rate):
+    global matrix
+    # Convert raw data (ASCII string) to numpy array
+    data = unpack("%dh"%(len(data)/2),data)
+    data = np.array(data, dtype='h')
+    # Apply FFT - real data
+    fourier=np.fft.rfft(data)
+    # Remove last element in array to make it the same size as chunk
+    fourier=np.delete(fourier,len(fourier)-1)
+    # Find average 'amplitude' for specific frequency ranges in Hz
+    power = np.abs(fourier)   
+    matrix[0]= int(np.mean(power[piff(0)    :piff(500):1]))
+    matrix[1]= int(np.mean(power[piff(500)  :piff(2000):1]))
+    matrix[2]= int(np.mean(power[piff(2000) :piff(20000):1]))
+    
+    matrix=np.divide(np.multiply(matrix,weighting),10000)
+
+   # Set floor at 0 and ceiling at 8 for LED matrix
+    matrix=matrix.clip(0,512)
+    return matrix
+
+# Main loop
+t = threading.Thread(target=serial_thread)
+t.start()
+
+while 1:
+    try:
+        # Get microphone data
+        data = stream.read(chunk)
+        matrix=calculate_levels(data, chunk,sample_rate)
+
+    except KeyboardInterrupt:
+        print("Ctrl-C Terminating...")
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        ser.close()
+        running = False
+        sys.exit(1)
+    except Exception, e:
+        print(e)
+        print("ERROR Terminating...")
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        ser.close()
+        running = False
+        sys.exit(1)
---
+++ makes/DaftPunkHelmet/mezzanine/helmet.ino
@@ -0,0 +1,446 @@
+#include <FastLED.h>
+
+#define DATA_LEFTEAR_PIN 1
+#define DATA_RIGTHEAR_PIN 2
+#define DATA_RIGHT_PIN 3
+#define DATA_LEFT_PIN 7
+#define BUTTON 4
+#define POT A0
+
+// Params for width and height
+const uint8_t kMatrixWidth = 4;
+const uint8_t kMatrixHeight = 8;
+const bool    kMatrixSerpentineLayout = true;
+
+#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
+#define NUM_LEDONE 1
+
+// Calculates position on string based on coordinates
+uint16_t XY( uint8_t x, uint8_t y)
+{
+  uint16_t i;
+  
+  if( kMatrixSerpentineLayout == false) {
+    i = (y * kMatrixWidth) + x;
+  }
+
+  if( kMatrixSerpentineLayout == true) {
+    if( y & 0x01) {
+      // Odd rows run backwards
+      uint8_t reverseX = (kMatrixWidth - 1) - x;
+      i = (y * kMatrixWidth) + reverseX;
+    } else {
+      // Even rows run forwards
+      i = (y * kMatrixWidth) + x;
+    }
+  }
+  
+  return i;
+}
+
+// Define the array of leds
+CRGB led_array[NUM_LEDS];
+CRGB led_ears[NUM_LEDONE];
+int boardLed = 13;
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(boardLed, OUTPUT);
+    pinMode(BUTTON, INPUT);
+    pinMode(POT, INPUT);
+    FastLED.addLeds<WS2812B, DATA_LEFT_PIN, GRB>(led_array, NUM_LEDS);
+    FastLED.addLeds<WS2812B, DATA_RIGHT_PIN, GRB>(led_array, NUM_LEDS);
+}
+
+char readIn;
+
+void clear()
+{
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        led_array[i] = CRGB::Black;
+    }
+    FastLED.show();
+}
+
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { led_array[i].nscale8(250); } }
+
+void cylon()
+{
+	static uint8_t hue = 0;
+
+	// First slide the led in one direction
+	for(int i = 0; i < NUM_LEDS; i++)
+    {
+		led_array[i] = CHSV(hue++, 255, 255);
+
+		FastLED.show(); 
+
+		fadeall();
+
+		delay(10);
+	}
+
+	// Now go in the other direction.  
+	for(int i = (NUM_LEDS)-1; i >= 0; i--)
+    {
+		led_array[i] = CHSV(hue++, 255, 255);
+		FastLED.show();
+
+		fadeall();
+
+		delay(10);
+	}
+}
+
+void lightRow(int row, CRGB color)
+{
+    for (int i = 0; i < kMatrixWidth; i++)
+    {
+        led_array[XY(i, row)] = color;                
+    }
+    FastLED.show();
+}
+
+CRGB rainbow[8] = {CRGB::Purple, CRGB::Blue, CRGB::PowderBlue,
+                   CRGB::Green, CRGB::LightGreen, CRGB::Yellow,
+                   CRGB::Orange, CRGB::Red};
+
+void lightRows(int rows, CRGB color)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        lightRow(i, color);
+    }
+}
+
+void fullColor(CRGB color) // this works
+{
+    lightRows(kMatrixHeight, color);
+}
+
+void fullonRainbow() // this works
+{
+    for (int i = 0; i < kMatrixHeight; i++)
+    {
+        lightRow(i, rainbow[i]);
+    }
+}
+
+void rainbowDrop()
+{
+    // drop in
+    for (int i = 0; i < kMatrixHeight; i++)
+    {
+        CRGB currentColor = rainbow[kMatrixHeight - i];
+        
+        int currentPosition = kMatrixHeight;
+        int previousPosition = currentPosition + 1;
+
+        while(currentPosition > i)
+        {
+            currentPosition = currentPosition - 1;
+            previousPosition = currentPosition + 1;
+
+            lightRow(currentPosition, currentColor);
+            if (previousPosition <= (kMatrixHeight - 1))
+            {
+                lightRow(previousPosition, CRGB::Black);
+            }
+            FastLED.show();
+            delay(100);          
+        }
+    }
+
+    // clear out
+    for (int i = 0; i < kMatrixHeight; i++)
+    {
+        CRGB currentColor = rainbow[kMatrixHeight - i];
+        
+        int currentPosition = i;
+        int previousPosition = currentPosition - 1;
+
+        while(currentPosition >= -1)
+        {
+            currentPosition = currentPosition - 1;
+            previousPosition = currentPosition + 1;
+
+            if (currentPosition != -1)
+            {
+                lightRow(currentPosition, currentColor);
+            }
+            lightRow(previousPosition, CRGB::Black);
+
+            FastLED.show();
+            delay(100);          
+        }
+    }
+}
+
+int cycleColor = 0;
+void rainbowCycle()
+{
+    CRGB currentColor = rainbow[cycleColor];
+    cycleColor++;
+
+    if (cycleColor > 7)
+    {
+        cycleColor = 0;
+    }
+
+    int currentPosition = kMatrixHeight;
+    int previousPosition = currentPosition + 1;
+
+    while(currentPosition > -1)
+    {
+        currentPosition = currentPosition - 1;
+        previousPosition = currentPosition + 1;
+
+        if (currentPosition > -1)
+        {
+            lightRow(currentPosition, currentColor);            
+        }
+        if (previousPosition <= (kMatrixHeight - 1))
+        {
+            lightRow(previousPosition, CRGB::Black);
+        }
+        FastLED.show();
+        delay(100);          
+    }
+}
+
+void bounce()
+{
+    CRGB currentColor = rainbow[cycleColor];
+    cycleColor++;
+
+    if (cycleColor > 7)
+    {
+        cycleColor = 0;
+    }
+
+    int bounceDistance = 3;
+    int bottomArray[] = {0, 1, 2};
+    int topArray[] = {kMatrixHeight - 1,  kMatrixHeight - 2, kMatrixHeight -3};
+
+    for (int i = 0; i < bounceDistance; i++)
+    {
+        lightRow(topArray[i], currentColor);    
+        lightRow(bottomArray[i], currentColor);
+        FastLED.show();
+        delay(100);       
+    }
+
+    for (int i = bounceDistance; i >= 0; i--)
+    {
+        lightRow(topArray[i], CRGB::Black);    
+        lightRow(bottomArray[i], CRGB::Black);
+        FastLED.show();
+        delay(100);       
+    }
+}
+
+enum Mode
+{
+    Solid,
+    Animation1,
+    Animation2,
+    Animation3,
+    Animation4,
+    Audio1,
+    Audio2,
+    Audio3
+};
+
+Mode mode = Animation2;
+int low, mid, high;
+int bassCount = 0;
+void loop() {
+    while (Serial.available() > 0)
+    {
+        readIn = Serial.read();
+        if (readIn == 's')
+        {
+            low = Serial.parseInt();
+            mid = Serial.parseInt();
+            high = Serial.parseInt();
+            break;
+        }    
+    }
+
+    // use pot to select mode
+    int potVal = analogRead(POT);
+    Mode previousMode = mode;
+    if (potVal < 128)
+    {
+        mode = Solid;
+    }
+    else if (potVal >= 128 && potVal < 256)
+    {
+        mode = Animation1;
+    }
+    else if (potVal >= 256 && potVal < 384)
+    {
+        mode = Animation2;
+    }
+    else if (potVal >= 384 && potVal < 512)
+    {
+        mode = Animation3;
+    }
+    else if (potVal >= 512 && potVal < 640)
+    {
+        mode = Animation4;
+    }
+    else if (potVal >= 640 && potVal < 768)
+    {
+        mode = Audio1;
+    }
+    else if (potVal >= 768 && potVal < 896)
+    {
+        mode = Audio2;
+    }
+    else if (potVal >= 896)
+    {
+        mode = Audio3;
+    }
+
+    if (mode != previousMode)
+    {
+        bassCount++;
+    }
+
+    // lighting mode
+    switch(mode)
+    {
+    case Solid:
+    {
+        fullonRainbow();
+        break;
+    }
+    case Animation1:
+    {
+        cylon();
+        break;
+    }
+    case Animation2:
+    {
+        clear();
+        bounce();
+        clear();
+        delay(1000);
+        break;
+    }
+    case Animation3:
+    {
+        clear();
+        rainbowDrop();
+        clear();
+        break;
+    }
+    case Animation4:
+    {
+        clear();
+        rainbowCycle();
+        clear();
+        delay(1000);
+        break;
+    }
+    case Audio1:
+    {
+        clear();
+
+        if (low > 20)
+        {
+            bassCount++;
+        }
+
+        int colorChoice = bassCount % mode;
+        CRGB bassColor = rainbow[colorChoice];
+
+	if (mid > 800)
+	{
+        	lightRows(8, bassColor);
+	}
+        else if (mid > 700)
+	{
+        	lightRows(7, bassColor);
+	}
+	else if (mid > 600)
+        {
+        	lightRows(6, bassColor);
+        }
+        else if (mid > 500)
+        {
+        	lightRows(5, bassColor);
+        }
+        else if (mid > 400)
+        {
+        	lightRows(4, bassColor);
+        }
+        else if (mid > 300)
+        {
+        	lightRows(3, bassColor);
+        }
+        else if (mid > 200)
+        {
+        	lightRows(2, bassColor);
+        }
+        else if (mid > 100)
+        {
+        	lightRows(1, bassColor);
+        }
+        break;
+    }
+    case Audio2:
+    {
+        clear();
+
+        if (mid > 20)
+        {
+            bassCount++;
+        }
+
+        int colorChoice = bassCount % mode;
+        CRGB bassColor = rainbow[colorChoice];
+
+        if (low > 20)
+	{
+        	lightRows(8, bassColor);
+	}
+        else if (low > 15)
+        {
+        	lightRows(6, bassColor);
+        }
+        else if (low > 10)
+        {
+        	lightRows(4, bassColor);
+        }
+        else
+        {
+        	lightRows(2, bassColor);
+        }
+        break;
+    }
+    case Audio3:
+    {
+        clear();
+
+        if (mid > 30)
+        {
+            bassCount++;
+        }
+
+        int colorChoice = bassCount % mode;
+        CRGB bassColor = rainbow[colorChoice];
+
+        fullColor(bassColor);
+    }
+    }
+
+    if (bassCount > 8000)
+    {
+        bassCount = 0;
+    }
+
+}
