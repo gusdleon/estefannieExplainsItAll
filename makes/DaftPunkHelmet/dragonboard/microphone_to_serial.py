7f37da07fbde7e24276e0b50cdc38a0875229d19
Estefannie Explains It All <estefanniegg@users.noreply.github.com>
2018-08-01 20:40:31 -0500

    Updated comments

--- makes/DaftPunkHelmet/dragonboard/microphone_to_serial.py
+++ makes/DaftPunkHelmet/dragonboard/microphone_to_serial.py
@@ -1,3 +1,14 @@
+# DAFT PUNK HELMET
+#  
+# Have fun =)
+#
+# This file is part of the Estefannie Explains It All repo.
+#
+# (c) Estefannie Explains It All <estefannieexplainsitall@gmail.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
 import sys
 import pyaudio
 from struct import unpack
